6dd8d234ef7fa0b4b46c8cc017747e302e07e5b9
Darren Duncan <darren@DarrenDuncan.net>
2017-06-06 14:38:12 -0700

    add canonical copy of Muldis.DBP / IMDBP.cs interface library

--- README.md
+++ README.md
@@ -1,13 +1,30 @@
 # NAME
 
-Muldis::DBP -
+Muldis.DBP -
 Formal spec of an abstract database protocol for .Net
+
+# VERSION
+
+This document describes Muldis.DBP version 0.
 
 # DESCRIPTION
 
-This distribution features the pure documentation file named
+This distribution co-features the pure documentation file named
 Muldis::DBP (Formal spec of an abstract database protocol for .Net)
 and its support structure.
+
+This distribution co-features the C\# library named ```Muldis.DBP``` which
+is the official machine-readable set of .Net interface definitions
+corresponding to that pure documentation file.
+
+*This documentation is pending.*
+
+# DEPENDENCIES
+
+See Muldis.D.Ref\_Eng ([https://github.com/muldis/Muldis-D-Ref-DotNet](
+https://github.com/muldis/Muldis-D-Ref-DotNet)) which is the initial
+primary interface composer of Muldis.DBP and has the same DEPENDENCIES,
+as well as an application; Muldis.DBP in isolation can not be executed.
 
 # AUTHOR
 
@@ -15,6 +32,27 @@ Darren Duncan - darren@DarrenDuncan.net
 
 # LICENSE AND COPYRIGHT
 
-Muldis::DBP is Copyright © 2015-2017, Muldis Data Systems, Inc.
+Muldis.DBP is Copyright © 2015-2017, Muldis Data Systems, Inc.
 
 [http://www.muldis.com/](http://www.muldis.com/)
+
+Muldis.DBP is free software; you can redistribute it and/or modify
+it under the terms of the Artistic License version 2 (AL2) as published by
+the Perl Foundation ([http://www.perlfoundation.org/](
+http://www.perlfoundation.org/)).  You should have received a copy of the
+AL2 as part of the Muldis.DBP distribution, in the file named
+"LICENSE/artistic-2\_0.txt"; if not, see
+[http://www.perlfoundation.org/attachment/legal/artistic-2\_0.txt](
+http://www.perlfoundation.org/attachment/legal/artistic-2\_0.txt).
+
+Any versions of Muldis.DBP that you modify and distribute must carry
+prominent notices stating that you changed the files and the date of any
+changes, in addition to preserving this original copyright notice and other
+credits.  Muldis.DBP is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+While it is by no means required, the copyright holder of Muldis.DBP
+would appreciate being informed any time you create a modified version of
+Muldis.DBP that you are willing to distribute, because that is a
+practical way of suggesting improvements to the standard version.
---
+++ src/Muldis.DBP/IMDBP.cs
@@ -0,0 +1,219 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+
+[assembly: CLSCompliant(true)]
+
+namespace Muldis.DBP
+{
+    public interface IInfo
+    {
+        Boolean Provides_Dot_Net_Muldis_DBP();
+
+        IMachine Want_VM_API(Object requested_version);
+    }
+
+    public interface IMachine
+    {
+        IExecutor Executor();
+
+        IImporter Importer();
+    }
+
+    public interface IExecutor
+    {
+        IMachine Machine();
+
+        IMD_Any Evaluates(IMD_Any function, IMD_Any args = null);
+
+        void Performs(IMD_Any procedure, IMD_Any args = null);
+    }
+
+    public interface IImporter
+    {
+        IMachine Machine();
+
+        IMD_Any MD_Any(Object value);
+
+        IMD_Boolean MD_Boolean(Boolean value);
+
+        IMD_Integer MD_Integer(BigInteger value);
+
+        IMD_Integer MD_Integer(Int32 value);
+
+        IMD_Fraction MD_Fraction(IMD_Integer numerator, IMD_Integer denominator);
+
+        IMD_Fraction MD_Fraction(BigInteger numerator, BigInteger denominator);
+
+        IMD_Fraction MD_Fraction(Int32 numerator, Int32 denominator);
+
+        IMD_Fraction MD_Fraction(Decimal value);
+
+        IMD_Bits MD_Bits(BitArray members);
+
+        IMD_Blob MD_Blob(Byte[] members);
+
+        IMD_Text MD_Text(String members);
+
+        IMD_Array MD_Array(List<Object> members);
+
+        IMD_Set MD_Set(List<Object> members);
+
+        IMD_Bag MD_Bag(List<Object> members);
+
+        IMD_Tuple MD_Tuple(
+            Object a0 = null, Object a1 = null, Object a2 = null,
+            Nullable<KeyValuePair<String,Object>> attr = null,
+            Dictionary<String,Object> attrs = null);
+
+        IMD_Heading MD_Heading(
+            Nullable<Boolean> a0 = null, Nullable<Boolean> a1 = null,
+            Nullable<Boolean> a2 = null,
+            String attr_name = null, HashSet<String> attr_names = null);
+
+        IMD_Tuple_Array MD_Tuple_Array(IMD_Heading heading);
+
+        IMD_Tuple_Array MD_Tuple_Array(IMD_Array body);
+
+        IMD_Relation MD_Relation(IMD_Heading heading);
+
+        IMD_Relation MD_Relation(IMD_Set body);
+
+        IMD_Tuple_Bag MD_Tuple_Bag(IMD_Heading heading);
+
+        IMD_Tuple_Bag MD_Tuple_Bag(IMD_Bag body);
+
+        IMD_Interval MD_Interval(IMD_Any min, IMD_Any max,
+            Boolean excludes_min = false, Boolean excludes_max = false);
+
+        IMD_Capsule MD_Capsule(IMD_Any label, IMD_Tuple attrs);
+
+        IMD_Capsule MD_Capsule(String label, IMD_Tuple attrs);
+
+        IMD_Capsule MD_Capsule(String[] label, IMD_Tuple attrs);
+
+        IMD_Variable New_MD_Variable(IMD_Any initial_current_value);
+
+        IMD_Process New_MD_Process();
+
+        IMD_Stream New_MD_Stream();
+
+        IMD_External New_MD_External(Object value);
+
+        IMD_Excuse MD_Excuse(IMD_Tuple attrs);
+
+        IMD_Excuse MD_Excuse(String value);
+
+        IMD_Excuse_No_Reason MD_Excuse_No_Reason();
+
+        IMD_Heading MD_Attr_Name(String label);
+
+        IMD_Array MD_Attr_Name_List(String[] label);
+    }
+
+    public interface IMD_Any
+    {
+        IMachine Machine();
+    }
+
+    public interface IMD_Boolean : IMD_Any
+    {
+        Boolean Export_Boolean();
+    }
+
+    public interface IMD_Integer : IMD_Any
+    {
+        BigInteger Export_BigInteger();
+
+        Int32 Export_Int32();
+    }
+
+    public interface IMD_Fraction : IMD_Any
+    {
+    }
+
+    public interface IMD_Bits : IMD_Any
+    {
+    }
+
+    public interface IMD_Blob : IMD_Any
+    {
+    }
+
+    public interface IMD_Text : IMD_Any
+    {
+    }
+
+    public interface IMD_Array : IMD_Any
+    {
+    }
+
+    public interface IMD_Set : IMD_Any
+    {
+    }
+
+    public interface IMD_Bag : IMD_Any
+    {
+    }
+
+    public interface IMD_Tuple : IMD_Any
+    {
+    }
+
+    public interface IMD_Heading : IMD_Any
+    {
+    }
+
+    public interface IMD_Tuple_Array : IMD_Any
+    {
+    }
+
+    public interface IMD_Relation : IMD_Any
+    {
+    }
+
+    public interface IMD_Tuple_Bag : IMD_Any
+    {
+    }
+
+    public interface IMD_Interval : IMD_Any
+    {
+    }
+
+    public interface IMD_Capsule : IMD_Any
+    {
+    }
+
+    public interface IMD_Handle : IMD_Any
+    {
+    }
+
+    public interface IMD_Variable : IMD_Handle
+    {
+        IMD_Any Current();
+
+        void Assign(IMD_Any value);
+    }
+
+    public interface IMD_Process : IMD_Handle
+    {
+    }
+
+    public interface IMD_Stream : IMD_Handle
+    {
+    }
+
+    public interface IMD_External : IMD_Handle
+    {
+        Object Export_External_Object();
+    }
+
+    public interface IMD_Excuse : IMD_Any
+    {
+    }
+
+    public interface IMD_Excuse_No_Reason : IMD_Excuse
+    {
+    }
+}
